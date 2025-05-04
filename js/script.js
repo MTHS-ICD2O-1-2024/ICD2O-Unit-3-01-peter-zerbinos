778118ef8418625e021e8c82965c1ee43ff95e11
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-04 23:07:20 +0000

    Made edits to code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateAreaOfTrapezoid() {
+  const aBaseOfTrapezoid = parseFloat(document.getElementById('a-base-of-trapezoid').value)
+  const bBaseOfTrapezoid = parseFloat(document.getElementById('b-base-of-trapezoid').value)
+  const heightOfTrapezoid = parseFloat(document.getElementById('height-of-trapezoid').value)
+
+  const areaOfTrapezoid = ((aBaseOfTrapezoid + bBaseOfTrapezoid) / 2) * heightOfTrapezoid
+
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTrapezoid + ' mm²'
+}
